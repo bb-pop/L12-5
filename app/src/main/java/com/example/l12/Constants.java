c5e2c2a6e4e125102a89599522af805d5efea4e1
KUY <thanat.ti.64@ubu.ac.th>
2022-09-30 23:36:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/l12/Constants.java
@@ -0,0 +1,7 @@
+package com.example.l12;
+
+public class Constants {
+    public static final String STORAGE_PATH_UPLOADS = "foods/";
+    public static final String DATABASE_PATH_UPLOADS = "foods";
+}
+
